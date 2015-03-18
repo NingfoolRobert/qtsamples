19ba9191a1145ec09060ce119fb3ea9e65ce2b4e
sjw <sjw@ee.com>
2015-03-18 11:04:45 +0800

    initial commit

---
+++ chap02/find/build/CMakeFiles/find.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "moc_finddialog.cxx"
+  "CMakeFiles/find.dir/finddialog.o"
+  "CMakeFiles/find.dir/main.o"
+  "CMakeFiles/find.dir/moc_finddialog.o"
+  "find.pdb"
+  "find"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/find.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
